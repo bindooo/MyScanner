1c9641dbbb94d24591d6bb620f5ee09eab71b159
bindooo <bindicsgeri@gmail.com>
2015-09-30 18:06:31 +0200

    Row scan into string array included

--- app/src/main/java/com/faveoffate/myscanner/ScanResultActivity.java
+++ app/src/main/java/com/faveoffate/myscanner/ScanResultActivity.java
@@ -3,6 +3,7 @@ package com.faveoffate.myscanner;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ListAdapter;
@@ -18,27 +19,37 @@ public class ScanResultActivity extends Activity {
     protected ListAdapter adapter;
     protected ListView productList;
     protected String resultString;
+    protected String[] s1 = new String[2];
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_result);
 
+        String[] match;
+        TextView barcodeView = (TextView)findViewById(R.id.barcodeView);
+
         Intent i = getIntent();
         resultString = i.getStringExtra("extra_resultString");
 
         db = (new DataBaseHelper(this)).getReadableDatabase();
 
-        productList = (ListView) findViewById (R.id.productList);
+//        productList = (ListView) findViewById (R.id.productList);
+//        search(resultString);
 
-        search(resultString);
+        match = returnMatchByBarcode(resultString);
+
+        barcodeView.setText(match[0]);
 
     }
 
     void search(String resultString) {
-        // || is the concatenation operation in SQLite
+
         cursor = db.rawQuery("SELECT _id, barcode, product FROM Products WHERE barcode LIKE ?",
                 new String[]{resultString});
+
+        if (cursor==null || cursor.getCount()==0) setContentView(R.layout.no_result);
+
         adapter = new SimpleCursorAdapter(
                 this,
                 R.layout.product_list_item,
@@ -46,9 +57,22 @@ public class ScanResultActivity extends Activity {
                 new String[] {"barcode", "product"},
                 new int[] {R.id.barcode, R.id.product});
         productList.setAdapter(adapter);
-
-        if (cursor==null || cursor.getCount()==0) setContentView(R.layout.no_result);
-
     }
 
+    public String[] returnMatchByBarcode(String resultString) throws SQLException {
+
+        String[] columns = new String[]{ "barcode", "product" };
+        Cursor c = db.query("Products", columns, "barcode" + "=" + resultString, null, null, null, null);
+
+        if (c != null){
+            c.moveToFirst();
+            for(int i = 0; i<2; i++){
+                s1[i] = c.getString(i);
+            }
+            c.close();
+            return s1;
+        }
+
+        return null;
+    }
 }
