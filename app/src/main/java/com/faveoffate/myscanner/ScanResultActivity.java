14f0c4741e48f885e7a16f4ee37bd3b8e394ce0c
bindooo <bindicsgeri@gmail.com>
2015-09-30 17:11:00 +0200

    If match -> list, no match included

--- app/src/main/java/com/faveoffate/myscanner/ScanResultActivity.java
+++ app/src/main/java/com/faveoffate/myscanner/ScanResultActivity.java
@@ -2,32 +2,53 @@ package com.faveoffate.myscanner;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * Created by egerbin on 8/28/2015.
- */
 public class ScanResultActivity extends Activity {
+
+    protected SQLiteDatabase db;
+    protected Cursor cursor;
+    protected ListAdapter adapter;
+    protected ListView productList;
+    protected String resultString;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_result);
 
         Intent i = getIntent();
-        String resultString = i.getStringExtra("extra_resultString");
+        resultString = i.getStringExtra("extra_resultString");
 
-        TextView resultText;
+        db = (new DataBaseHelper(this)).getReadableDatabase();
 
-        resultText = (TextView)findViewById(R.id.resultText);
-        resultText.setTextSize(20);
-        resultText.setText(resultString);
+        productList = (ListView) findViewById (R.id.productList);
+
+        search(resultString);
 
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
+    void search(String resultString) {
+        // || is the concatenation operation in SQLite
+        cursor = db.rawQuery("SELECT _id, barcode, product FROM Products WHERE barcode LIKE ?",
+                new String[]{resultString});
+        adapter = new SimpleCursorAdapter(
+                this,
+                R.layout.product_list_item,
+                cursor,
+                new String[] {"barcode", "product"},
+                new int[] {R.id.barcode, R.id.product});
+        productList.setAdapter(adapter);
+
+        if (cursor==null || cursor.getCount()==0) setContentView(R.layout.no_result);
+
     }
 
 }
